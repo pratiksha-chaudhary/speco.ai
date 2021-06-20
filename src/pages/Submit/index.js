567ca13d64bd5b4eee060c75488986b14c96b4ec
Ravi Chaudhary <cravi24@gmail.com>
2021-06-20 15:58:53 +0200

    adding chnages to introduce templates (#1)

---
+++ src/pages/Submit/index.js
@@ -0,0 +1,11 @@
+import SubmitButton from '../../components/SubmitButton';
+
+const Submit = () => {
+  return (
+    <div>
+      Thanks<SubmitButton text="Back Home" nextRoute="/"></SubmitButton>
+    </div>
+  );
+};
+
+export default Submit;
--- src/pages/Survey/index.js
+++ src/pages/Survey/index.js
@@ -1,22 +1,31 @@
-import React, { useContext } from "react";
-import SubmitButton from "../../components/SubmitButton";
-import { useParams } from "react-router-dom";
-import UserContext from "../../context/userContext";
+import React, { useContext } from 'react';
+import SubmitButton from '../../components/SubmitButton';
+import { useParams } from 'react-router-dom';
+import UserContext from '../../context/userContext';
+import userType from '../../constants/user-type';
 import {
-    doctorSurveyQuestions,
-    patientSurveyQuestions,
-} from "./survey-questions";
-import "./index.scss";
+  doctorSurveyQuestions,
+  patientSurveyQuestions,
+} from './survey-questions';
+import './index.scss';
 
 function Survey() {
-    const { pageIndex } = useParams();
-    const { user } = useContext(UserContext);
-    const id = parseInt(pageIndex);
-    return (
-        <div className="Survey">
-            <h1>Survey {user}</h1>
-            <SubmitButton text="Next" nextRoute={`/survey/${id + 1}`} />
-        </div>
-    );
+  const { pageIndex } = useParams();
+  const { user } = useContext(UserContext);
+  const targetQuestions =
+    user === userType.PATIENT ? patientSurveyQuestions : doctorSurveyQuestions;
+  const id = parseInt(pageIndex);
+  return (
+    <div className="Survey">
+      <h1>Survey {user}</h1>
+      <div>{targetQuestions[pageIndex].question}</div>
+      {pageIndex < targetQuestions.length - 1 && (
+        <SubmitButton text="Next" nextRoute={`/survey/${id + 1}`} />
+      )}
+      {pageIndex >= targetQuestions.length - 1 && (
+        <SubmitButton text="Next" nextRoute="/submit" />
+      )}
+    </div>
+  );
 }
 export default Survey;
