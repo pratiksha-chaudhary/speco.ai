7f9b36162c59ba80b5c9eb8fb36433749083a210
Ravi Chaudhary <rchaudhary@babbel.com>
2021-06-29 00:29:36 +0200

    fixed unique key warning for progress-bar

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -3,12 +3,16 @@ import './index.scss';
 const ProgressBar = ({ currentProgress, total }) => {
   return (
     <div className="ProgressBar">
-      {Array(currentProgress).fill(true).map(() => (
-        <div className="current-progress" />
-      ))}
-      {Array(total - currentProgress).fill(true).map(() => (
-        <div className="left-progress" />
-      ))}
+      {Array(currentProgress)
+        .fill(true)
+        .map((cur, i) => (
+          <div className="current-progress" key={i} />
+        ))}
+      {Array(total - currentProgress)
+        .fill(true)
+        .map((cur, i) => (
+          <div className="left-progress" key={i} />
+        ))}
     </div>
   );
 };
