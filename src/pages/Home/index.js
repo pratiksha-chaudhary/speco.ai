de9bb764e50ed64e5ddb4db39907916857997e55
Ravi Chaudhary <rchaudhary@babbel.com>
2021-07-05 22:14:15 +0200

    added birthday node with date picker

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,11 +5,14 @@ import './index.scss';
 
 function Home() {
   return (
-    <div className="Home">
-      <Introduction />
-      <SubmitButton text="Start Now" nextRoute="/register" />
-      <p className="survey-time">Takes 1 min only!</p>
-    </div>
+    <>
+      
+      <div className="Home">
+        <Introduction />
+        <SubmitButton text="Start Now" nextRoute="/register" />
+        <p className="survey-time">Takes 1 min only!</p>
+      </div>
+    </>
   );
 }
 export default Home;
--- src/templates/Birthday/index.js
+++ src/templates/Birthday/index.js
@@ -1,57 +1,45 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect } from 'react';
+import DatePicker from 'react-datepicker';
 import Option from '../../components/Option';
 import UserContext from '../../context/userContext';
 import './index.scss';
+import 'react-datepicker/dist/react-datepicker.css';
 
-const isInvalid = (val, type) => {
-  const num = parseInt(val);
-
-  if (isNaN(num)) return true;
-  if (type === 'day') {
-    return num < 1 || num > 31;
+const calculate_age = (dob1) => {
+  var today = new Date();
+  var birthDate = new Date(dob1);
+  var age_now = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age_now--;
   }
-
-  if (type === 'month') {
-    return num < 1 || num > 12;
-  }
-
-  if (type === 'year') {
-    return num < 1000 || num > new Date().getFullYear();
-  }
+  return age_now;
 };
 
 const Birthday = ({ setResponse }) => {
   const { birthday, setBirthday } = useContext(UserContext);
-  const [error, setError] = useState(false);
 
   useEffect(() => {
     setResponse(birthday);
   }, [birthday, setResponse]);
 
-  const onChange = ({ target }) => {
-    setBirthday(target.value);
-    var date1 = target.value.split('/');
-    if (
-      date1.length < 3 ||
-      isInvalid(date1[0], 'day') ||
-      isInvalid(date1[1], 'month') ||
-      isInvalid(date1[2], 'year')
-    ) {
-      setError(true);
-      return;
-    }
-
-    var newDate = date1[1] + '/' + date1[0] + '/' + date1[2];
-
-    var date = new Date(newDate);
-
-    const parsedDate = date.toLocaleDateString('en-GB');
-    setResponse(parsedDate);
-    setError(false);
+  const setStartDate = (dob) => {
+    const age = calculate_age(dob);
+    setBirthday(dob);
+    setResponse(age);
   };
   return (
-    <Option className={`option-padding ${error ? 'error' : ''}`}>
-      <input onChange={onChange} placeholder="DD/MM/YYYY" value={birthday} />
+    <Option className="option-padding">
+      <DatePicker
+        selected={birthday}
+        dateFormat="dd/MM/yyyy"
+        placeholderText="DD/MM/YYYY"
+        peekNextMonth
+        showMonthDropdown
+        showYearDropdown
+        dropdownMode="select"
+        onChange={(date) => setStartDate(date)}
+      />
     </Option>
   );
 };
